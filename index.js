a4e2dec41d12b2afa2b7230a6737451ce561816b
mohamed yasser amer <my2722000@gmail.com>
2021-12-19 11:08:40 +0200

    fix the errors found in the files

--- index.js
+++ index.js
@@ -1,13 +1,1 @@
-let btn = document.querySelector("button");
-
-let k = 10;
-btn.onclick;
-btn.addEventListener("click", () => {
-  if (k < 0) {
-    alert("opopopoppop");
-  }
-});
-btn.addEventListener("click", () => {
-  k--;
-  alert("hahahahaha");
-});
+alert("Hi welcome to your new project");
