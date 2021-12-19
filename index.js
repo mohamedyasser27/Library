1c919b3348d03c6fb1c0ec0541a827febba86dbb
mohamed yasser amer <my2722000@gmail.com>
2021-12-19 10:30:15 +0200

    add v1 of the boilerplate

---
+++ index.js
@@ -0,0 +1,13 @@
+let btn = document.querySelector("button");
+
+let k = 10;
+btn.onclick;
+btn.addEventListener("click", () => {
+  if (k < 0) {
+    alert("opopopoppop");
+  }
+});
+btn.addEventListener("click", () => {
+  k--;
+  alert("hahahahaha");
+});
